200d0c17f06ec3efe6567f0e576d0df3e3eff7df
Yaris-Espitia <yespitiaplazas@correo.unicordoba.edu.co>
2021-09-09 21:03:43 -0500

    final

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log("Server listening at port", port);
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Chatroom
+
+let numUsers = 0;
+
+io.on("connection", (socket) => {
+  let addedUser = false;
+
+  socket.on("new message", (data) => {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit("new message", {
+      username: socket.username,
+      message: data,
+    });
+  });
+
+  socket.on("add user", (username) => {
+    if (addedUser) return;
+
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit("login", {
+      numUsers: numUsers,
+    });
+
+    socket.broadcast.emit("user joined", {
+      username: socket.username,
+      numUsers: numUsers,
+    });
+  });
+
+  socket.on("typing", () => {
+    socket.broadcast.emit("typing", {
+      username: socket.username,
+    });
+  });
+
+  socket.on("stop typing", () => {
+    socket.broadcast.emit("stop typing", {
+      username: socket.username,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    if (addedUser) {
+      --numUsers;
+
+      socket.broadcast.emit("user left", {
+        username: socket.username,
+        numUsers: numUsers,
+      });
+    }
+  });
+});
